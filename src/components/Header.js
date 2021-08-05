56f923fe4774f0cfca883b626c2fc04b5522abf8
leo <lolkaazhaha@gmail.com>
2021-08-05 11:45:02 +0400

    add some login btn styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -130,7 +130,20 @@ const NavMenu = styled.div`
   }
 `
 const Login = styled.a`
+  cursor: pointer;
   background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease 0s;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+  }
 `
 
 export default Header
